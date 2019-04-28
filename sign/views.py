119137a78458d9c53dd8a76033740acebdc7bc43
computer-PC\computer <zhanghongkai@brzhongyi.com>
2019-04-28 15:38:40 +0800

    添加接口文档

--- sign/views.py
+++ sign/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.contrib import auth
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
